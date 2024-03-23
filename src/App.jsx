33e7bf5016bee07897212ff3a02a2dac50fd3607
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-23 10:41:55 -0300

    Se completa las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
+import Layout from './Components/Layout';
+import Contact from './Routes/Contact';
+import Favs from './Routes/Favs';
+import Detail from './Routes/Detail';
+import Home from "./Routes/Home";
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <BrowserRouter>
+      <Routes>
+
+        <Route element={<Layout />} >
+            <Route path="/" element={<Home/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/favs" element={<Favs/>}/>
+            <Route path="/detail/:id" element={<Detail/>}/>
+        </Route>
+
+        <Route path="*" element={<h1>404 not found</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
