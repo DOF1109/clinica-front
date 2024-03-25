ba4a9899f3a37a6331da300ce33c05dac9daaf20
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-25 16:02:20 -0300

    Cargo los datos correctamente en las Cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,11 +10,19 @@ const Card = ({ name, username, id }) => {
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
+        <img 
+          src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.flaticon.es%2Ficono-gratis%2Fusuario_747545&psig=AOvVaw16l6wBK78wGQ5SbxfI7ZVX&ust=1711479366226000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCMDyidmLkIUDFQAAAAAdAAAAABAE" 
+          alt="Foto de perfil"
+          width={100} />
+        
+        <p>{name}</p>
+
+        <p>{username}</p>
 
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+        <button onClick={addFav} className="favButton">⭐</button>
     </div>
   );
 };
