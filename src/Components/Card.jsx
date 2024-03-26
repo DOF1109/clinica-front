d94c07941f90ef607ba9738d8cbafc6b7690ad07
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-26 20:12:30 -0300

    Se corrige el Link de Card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const Card = ({ name, username, id }) => {
-
-  const addFav = ()=>{
+  const addFav = () => {
     // Aqui iria la logica para agregar la Card en el localStorage
-  }
+  };
 
   return (
-    <Link to={`/detail/${id}`}>
-      <div className="card">
-          <img 
-            src='./images/doctor.jpg'
-            alt="Foto de perfil"
-            width={150} />
+    <div className="card">
+      <Link to={`/detail/${id}`}>
+        <img src="./images/doctor.jpg" alt="Foto de perfil" width={150} />
 
-          <h3>{name}</h3>
+        <h3>{name}</h3>
 
-          <p>{id} - {username}</p>
+        <p>
+          {id} - {username}
+        </p>
 
-          {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
+        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
-          {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-          <button onClick={addFav} className="favButton">⭐</button>
-      </div>
-    </Link>
+        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+      </Link>
+      <button onClick={addFav} className="favButton">
+        ⭐
+      </button>
+    </div>
   );
 };
 
