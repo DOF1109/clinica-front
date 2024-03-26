92b8089a05f4446ede86408b37a5755b28c98443
DOF1109 <friasdaniel.1109@gmail.com>
2024-03-25 21:15:50 -0300

    Se agregan los links del Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
-
   return (
     <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
+      <img src="./images/DH.ico" alt="Logo DH" />
+      <Link to="/">Home</Link>
+      <Link to="/contact">Contact</Link>
+      <Link to="/favs">Favs</Link>
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
       <button>Change theme</button>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
